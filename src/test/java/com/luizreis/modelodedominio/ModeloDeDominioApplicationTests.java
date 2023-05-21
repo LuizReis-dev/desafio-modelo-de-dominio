419417a44ec1e27587e0584ce4c612a6319d0c17
Luiz Reis <reisluiz05@gmail.com>
2023-05-21 16:24:37 -0300

    Project finished

---
+++ src/test/java/com/luizreis/modelodedominio/ModeloDeDominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizreis.modelodedominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloDeDominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
